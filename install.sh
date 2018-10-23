f957ac925b93d00eac8938d627c8728f97a04d68
Bombwhale <solkblte@icloud.com>
2018-10-23 22:56:52 +0900

    fcitx skin settings etc.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-echo "Installing Neovim, fcitx, git and make..."
-sudo pacman -S neovim fcitx git make
+echo "Installing Neovim, fcitx, git, make and yay..."
+sudo pacman -S neovim fcitx git make yay
 
 echo "Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"<Paste>
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Installing Vundle.vim..."
 mkdir ~/.vim
@@ -12,14 +12,20 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "Copying config files to home directory..."
 cp ./home/.vimrc ~/.vimrc
 cp ./home/.zshrc ~/.zshrc
-cp -r .config ~
+echo "fcitx..."
+cp ./home/.config/fctix/skin/default ~/.config/fcitx/skin/ 
+echo "nvim..."
+cp -r ./home/.config/nvim/ ~/.config
+echo "agnoster..."
 cp ./home/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
+echo
 
 echo "Installing Golang 1.11.1..."
 wget https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.11.1.linux-amd64.tar.gz
 
 echo "Running :PluginInstall..."
-nvim +PluginInstall +qall
+nvim +PluginInstall +GoInstallBinaries +qall
 
 echo "install.sh done.\nDon't forget to edit 'bombwhale' in ~/.zshrc to your username."
+echo "After Edit it, run source ~/.zshrc"
